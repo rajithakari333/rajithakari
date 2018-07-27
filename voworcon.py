066f6279af7307be9e4b493d8eac283e6573be51
rajithakari333 <41767208+rajithakari333@users.noreply.github.com>
2018-07-27 16:13:16 +0530

    Update voworcon.py

--- voworcon.py
+++ voworcon.py
@@ -1,5 +1,7 @@
 str1=(input(""))
 if str1 in ('a','e','i','o','u','A','E','I','O','U'):
     print("Vowel")
-else:
+elif((str1>='a' and str1<='z') or (str1>='A' and str1<='Z')):
     print("Consonant")
+else:
+    print("Invalid")
