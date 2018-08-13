2a501492366efa217db05da7c95677a54a4798b9
rajithakari333 <41767208+rajithakari333@users.noreply.github.com>
2018-08-13 10:21:57 +0530

    Create sumofk.py

---
+++ sumofk.py
@@ -0,0 +1,12 @@
+a=[1,2,3,4,5,6,7,8,9]
+sum=0
+n=0
+k=0
+i=0
+n=int(input(""))
+k=int(input(""))
+for i in range(0,n):
+    print(a[i])
+for i in range(0,k):
+    sum=sum+a[i]
+print("\noutput:\n",sum)
