a569dd586089253546cf6d8b61f58dbe9bbc7bb2
rajithakari333 <41767208+rajithakari333@users.noreply.github.com>
2018-08-17 09:32:28 +0530

    Create sumofarith.py

---
+++ sumofarith.py
@@ -0,0 +1,12 @@
+def sumofarith(a,d,n):
+    sum=0
+    i=0
+    while(i<n):
+        sum=sum+a
+        a=a+d
+        i=i+1
+    return sum
+n=int(input(""))
+a=int(input(" "))
+d=int(input(" "))
+print(sumofarith(a,d,n))
